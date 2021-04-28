464d822a864563ebfabcca5507e176ea0ed40adf
Kvetoslav Bartos <>
2021-04-28 23:57:38 +0200

    build instructions

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# FK x GF Schoolbook Font
+# Montagu Slab
+
+WIP
+
+## Pre-building the Fonts
+
+Before building the fonts, it is necessary to decompose all smart components inside the Glyphs app.
+
+First, install "Pre-build" script placed in sources/glyphs-decomposed:
+
+1. Shift+Cmd+Y
+2. place "Pre-build.py" file into the "Scripts" folder
+3. restart Glyphs app
+
+Duplicate "Montagu-Slab.glyphs" into "sources/glyphs-decomposed", select all glyphs (Cmd+A) and run the script (under Script menu).
+
+Tested with Glyphs v2.6.6
+
+## Building the Fonts
+
+Family is built using fontmake and gftools post processing script. Tools are all python based.
+
+To install all the Python tools into a virtualenv, do the following:
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Then run the build script in the terminal:
+
+```
+cd sources/glyphs-decomposed
+sh build.sh
+```
+
+## License
+
+Montagu Slab is licensed under the SIL Open Font License v1.1, see [OFL.txt](OFL.txt) for details.
+
+## About Author
+
+Florian Karsten Studio (Brno, Czech Republic) focuses on graphic design, type design and programming. We create websites, books, programmes, typefaces and above all, functional systems. We're excited about open-source and peer2peer networks.
+
+Questions, comments, requests, suggestions: fonts@floriankarsten.com
--- sources/build.sh
+++ sources/build.sh
@@ -10,40 +10,40 @@ mkdir -p ../fonts/otf ../fonts/ttf ../fonts/ttf/static ../fonts/woff2 ../fonts/w
 echo "Generating Static OTFs"
 fontmake -g glyphs-decomposed/$font_file.glyphs -i -o otf --output-dir ../fonts/otf
 
-# echo "Generating Static TTFs"
-# fontmake -g glyphs-decomposed/$font_file.glyphs -i -o ttf --output-dir ../fonts/ttf/static
+echo "Generating Static TTFs"
+fontmake -g glyphs-decomposed/$font_file.glyphs -i -o ttf --output-dir ../fonts/ttf/static
 
-# echo "Generating VFs"
-# fontmake -g glyphs-decomposed/$font_file.glyphs -o variable --output-path ../fonts/ttf/$VF_file
+echo "Generating VFs"
+fontmake -g glyphs-decomposed/$font_file.glyphs -o variable --output-path ../fonts/ttf/$VF_file
 
-# rm -rf master_ufo/ instance_ufo/
+rm -rf master_ufo/ instance_ufo/
 
-# echo "Post processing"
+echo "Post processing"
 
-# ttfs=$(ls ../fonts/ttf/static/*.ttf)
-# for ttf in $ttfs
-# do
-# 	gftools fix-dsig -f $ttf;
-# 	ttfautohint $ttf $ttf.fix
-# 	mv "$ttf.fix" $ttf
-# 	gftools fix-hinting $ttf
-# 	mv "$ttf.fix" $ttf
-# 	woff2_compress $ttf
-# done
+ttfs=$(ls ../fonts/ttf/static/*.ttf)
+for ttf in $ttfs
+do
+	gftools fix-dsig -f $ttf;
+	ttfautohint $ttf $ttf.fix
+	mv "$ttf.fix" $ttf
+	gftools fix-hinting $ttf
+	mv "$ttf.fix" $ttf
+	woff2_compress $ttf
+done
 
-# vfs=$(ls ../fonts/ttf/*.ttf)
-# for vf in $vfs
-# do
-# 	gftools fix-dsig -f $vf;
-# 	gftools fix-nonhinting $vf "$vf.fix"
-# 	mv "$vf.fix" $vf
-# 	gftools fix-unwanted-tables --tables MVAR $vf
-# 	woff2_compress $vf
-# done
-# rm ../fonts/ttf/*backup*.ttf
+vfs=$(ls ../fonts/ttf/*.ttf)
+for vf in $vfs
+do
+	gftools fix-dsig -f $vf;
+	gftools fix-nonhinting $vf "$vf.fix"
+	mv "$vf.fix" $vf
+	gftools fix-unwanted-tables --tables MVAR $vf
+	woff2_compress $vf
+done
+rm ../fonts/ttf/*backup*.ttf
 
-# mv ../fonts/ttf/*.woff2 ../fonts/woff2
-# mv ../fonts/ttf/static/*.woff2 ../fonts/woff2/static
+mv ../fonts/ttf/*.woff2 ../fonts/woff2
+mv ../fonts/ttf/static/*.woff2 ../fonts/woff2/static
 
-# echo "Voila! Done."
-# cd ..
+echo "Voila! Done."
+cd ..

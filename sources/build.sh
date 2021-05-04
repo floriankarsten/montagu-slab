c636af9c0e6ac8b3c8c18ca9a9c1aac0841b958b
Kvetoslav Bartos <>
2021-05-04 22:10:03 +0200

    build update > modify VF fvar table

--- sources/build.sh
+++ sources/build.sh
@@ -2,7 +2,8 @@
 #!/bin/sh
 set -e
 
-mkdir -p ../fonts/otf ../fonts/ttf ../fonts/ttf/static ../fonts/woff2 ../fonts/woff2/static
+rm -rf reports ../fonts
+mkdir -p reports ../fonts/otf ../fonts/ttf ../fonts/ttf/static ../fonts/woff2 ../fonts/woff2/static
 
 echo "Generating UFOs"
 glyphs2ufo glyphs-decomposed/MontaguSlab.glyphs --generate-GDEF
@@ -17,26 +18,35 @@ mv $VF_File.fix $VF_File
 gftools fix-dsig -f $VF_File
 gftools fix-unwanted-tables $VF_File -t MVAR
 python3 MontaguSlab-STAT.py $VF_File
+ttx -m $VF_File MontaguSlab-fvar.ttx
+mv *.ttf ../fonts/ttf
+rm $VF_File
+mv ../fonts/ttf/MontaguSlab-fvar.ttf $VF_File
 fonttools ttLib.woff2 compress $VF_File
-
 mv ../fonts/ttf/*.woff2 ../fonts/woff2
 
-echo "Generating static TTFs"
-fontmake -m MontaguSlab.designspace -i -o ttf --output-dir ../fonts/ttf/static/ -a
+# echo "Generating static TTFs"
+# fontmake -m MontaguSlab.designspace -i -o ttf --output-dir ../fonts/ttf/static/ -a
 
-echo "Post processing static TTFs"
-ttfs=$(ls ../fonts/ttf/static/*.ttf)
-for ttf in $ttfs
-do
-	gftools fix-dsig -f $ttf;
-	gftools fix-hinting $ttf
-	mv "$ttf.fix" $ttf
-	fonttools ttLib.woff2 compress $ttf
-done
-mv ../fonts/ttf/static/*.woff2 ../fonts/woff2/static
+# echo "Post processing static TTFs"
+# ttfs=$(ls ../fonts/ttf/static/*.ttf)
+# for ttf in $ttfs
+# do
+# 	gftools fix-dsig -f $ttf;
+# 	gftools fix-hinting $ttf
+# 	mv "$ttf.fix" $ttf
+# 	fonttools ttLib.woff2 compress $ttf
+# done
+# mv ../fonts/ttf/static/*.woff2 ../fonts/woff2/static
 
 echo "Cleaning"
 rm -rf instance_ufos glyphs-decomposed/*.ufo glyphs-decomposed/MontaguSlab.designspace ../fonts/ttf/*backup*.ttf
+
+#echo "Reporting tables"
+
+#ttx -o report-fvar.ttx -t fvar $VF_File
+#ttx -o report-STAT.ttx -t STAT $VF_File
+#mv *.ttx reports
 
 echo "Voila! Done."
 cd ..

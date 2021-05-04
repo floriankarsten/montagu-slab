3fe0ca367a239108da816c9cb481ca91851236af
Kvetoslav Bartos <>
2021-05-05 00:41:14 +0200

    VF family name fix

--- sources/build.sh
+++ sources/build.sh
@@ -2,8 +2,8 @@
 #!/bin/sh
 set -e
 
-rm -rf reports ../fonts
-mkdir -p reports ../fonts/otf ../fonts/ttf ../fonts/ttf/static ../fonts/woff2 ../fonts/woff2/static
+rm -rf ../fonts
+mkdir -p ../fonts/otf ../fonts/ttf ../fonts/ttf/static ../fonts/woff2 ../fonts/woff2/static
 
 echo "Generating UFOs"
 glyphs2ufo glyphs-decomposed/MontaguSlab.glyphs --generate-GDEF
@@ -22,28 +22,31 @@ ttx -m $VF_File MontaguSlab-fvar.ttx
 mv *.ttf ../fonts/ttf
 rm $VF_File
 mv ../fonts/ttf/MontaguSlab-fvar.ttf $VF_File
+python3 scripts/fix-font-name.py $VF_File "Montagu Slab"
 fonttools ttLib.woff2 compress $VF_File
 mv ../fonts/ttf/*.woff2 ../fonts/woff2
 
-# echo "Generating static TTFs"
-# fontmake -m MontaguSlab.designspace -i -o ttf --output-dir ../fonts/ttf/static/ -a
+echo "Generating static TTFs"
+fontmake -m MontaguSlab.designspace -i -o ttf --output-dir ../fonts/ttf/static/ -a
 
-# echo "Post processing static TTFs"
-# ttfs=$(ls ../fonts/ttf/static/*.ttf)
-# for ttf in $ttfs
-# do
-# 	gftools fix-dsig -f $ttf;
-# 	gftools fix-hinting $ttf
-# 	mv "$ttf.fix" $ttf
-# 	fonttools ttLib.woff2 compress $ttf
-# done
-# mv ../fonts/ttf/static/*.woff2 ../fonts/woff2/static
+echo "Post processing static TTFs"
+ttfs=$(ls ../fonts/ttf/static/*.ttf)
+for ttf in $ttfs
+do
+	gftools fix-dsig -f $ttf;
+	gftools fix-hinting $ttf
+	mv "$ttf.fix" $ttf
+	fonttools ttLib.woff2 compress $ttf
+done
+mv ../fonts/ttf/static/*.woff2 ../fonts/woff2/static
+
+echo "Generating Static OTFs"
+fontmake -m MontaguSlab.designspace -i -o otf --output-dir ../fonts/otf
 
 echo "Cleaning"
 rm -rf instance_ufos glyphs-decomposed/*.ufo glyphs-decomposed/MontaguSlab.designspace ../fonts/ttf/*backup*.ttf
 
 #echo "Reporting tables"
-
 #ttx -o report-fvar.ttx -t fvar $VF_File
 #ttx -o report-STAT.ttx -t STAT $VF_File
 #mv *.ttx reports
@@ -51,44 +54,4 @@ rm -rf instance_ufos glyphs-decomposed/*.ufo glyphs-decomposed/MontaguSlab.desig
 echo "Voila! Done."
 cd ..
 
-
-
-
-# echo "Generating Static OTFs"
-# fontmake -g glyphs-decomposed/MontaguSlab.glyphs -i -o otf --output-dir ../fonts/otf
-
-# echo "Generating Static TTFs"
-# fontmake -g glyphs-decomposed/MontaguSlab.glyphs -i -o ttf --output-dir ../fonts/ttf/static
-
-# echo "Generating VFs"
-# fontmake -g glyphs-decomposed/MontaguSlab.glyphs -o variable --output-path ../fonts/ttf/MontaguSlab\[opsz,wght\].ttf
-
-# #rm -rf master_ufo/ instance_ufo/
-
-# echo "Post processing"
-
-# ttfs=$(ls ../fonts/ttf/static/*.ttf)
-# for ttf in $ttfs
-# do
-# 	gftools fix-dsig -f $ttf;
-# 	ttfautohint $ttf $ttf.fix
-# 	mv "$ttf.fix" $ttf
-# 	gftools fix-hinting $ttf
-# 	mv "$ttf.fix" $ttf
-# 	woff2_compress $ttf
-# done
-
-# vfs=$(ls ../fonts/ttf/*.ttf)
-# for vf in $vfs
-# do
-# 	gftools fix-dsig -f $vf;
-# 	gftools fix-nonhinting $vf "$vf.fix"
-# 	mv "$vf.fix" $vf
-# 	gftools fix-unwanted-tables --tables MVAR $vf
-# 	woff2_compress $vf
-# done
-# rm ../fonts/ttf/*backup*.ttf
-
-# mv ../fonts/ttf/*.woff2 ../fonts/woff2
-# mv ../fonts/ttf/static/*.woff2 ../fonts/woff2/static
-
+# fontbakery check-googlefonts *.ttf --succinct --loglevel WARN --ghmarkdown report.md
